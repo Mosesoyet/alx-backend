8df0c75f9b84a0267c548a00a97138ec8b02c93e
= <=>
2023-01-25 20:36:42 +0000

    Modify

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python3
-"""
-Task 1's modules.
+""" 
+0. Basic dictionary
 """
 
 
 BaseCaching = __import__("base_caching").BaseCaching
 
 
-class BasicCaching(BaseCaching):
-    """BasicCache class
-    """
+class BasicCache(BaseCaching):
+    ''' 
+    BasicCache class. Inherits from BaseCaching.
+    '''
 
-    def put(self, key, item):
-        """ Must assign to the directory base.cache_data the item value for
-        the key, if key or item is None then take no action
-        """
-        if key and item:
-            self.cache_data[key] = item
+    def put(self, key, value):
+        ''' Must assign to the dictionary self.cache_data the item value for
+        the key key. If key or item is None, this method should not do anything
+        '''
+        if key and value:
+            self.cache_data[key] = value
 
     def get(self, key):
-        """ return the value in self.cache_data linked to key.
-        If key is None or dosent exists, return  None
-        """
+        '''
+        Must return the value in self.cache_data linked to key. If key is
+        None or if the key doesn’t exist in self.cache_data, return None.
+        '''
         return self.cache_data.get(key, None)
