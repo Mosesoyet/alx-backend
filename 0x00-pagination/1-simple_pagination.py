15cb7ba18172b63c483225f48f04d9ae8b237b0d
= <=>
2023-01-25 20:15:34 +0000

    Edit firstline comment

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+o#!/usr/bin/env python3
 """
 Task 0's module
 """
 import csv
 import math
-from typing import Tuple
+from typing import Tuple, List
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -13,26 +13,24 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     and an end index corresponding to the range of indexs 
     to return in a list for those particular pagination parameters
     """
-    start_index, end_index = 0, 0
+    start_index = 0
+    end_index = 0
     for i in range(page):
         start_index = end_index
         end_index += page_size
-
     return (start_index, end_index)
 
 
 class Server:
-    """Server class to paginate database
-    of popular baby name
+    """Server class to paginate a database of popular baby names.
     """
-     DATA_FILE = "Popular_Baby_Names.csv"
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -43,16 +41,4 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        dataset page
-        """
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
-
-        dataset = self.dataset()
-        data_length = len(dataset)
-        try:
-            index = index_range(page, page_size)
-            return dataset[index[0]:index[1]]
-        except IndexError:
-            return []
+            pass
