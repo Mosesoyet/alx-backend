4bcad26a99e9a766c65e67f5faed8ac10927e3e3
= <=>
2023-01-31 22:01:47 +0200

    Modify

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,30 +2,17 @@
 """
 app's module
 """
-from flask import Flask, render_template, request
-from flask_babel import Babel
-
-
-class Config(object):
-    """ A config class for our app.
-    This configures the languages that the app supports
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCAL = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object(Config)
 app.url_map.strict_slashes = False
-babel = Babel(app)
 
 
 @app.route('/')
 def index():
     """ The home page for the website
     """
-    return render_template("1-index.html")
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
