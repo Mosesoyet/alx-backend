72268e48ebdeedf83f68950a0182a60dbf99d303
= <=>
2023-02-01 01:11:25 +0200

    modify

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -39,7 +39,7 @@ def get_locale():
 def index():
     """ The home page for the website
     """
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
