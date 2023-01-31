8237e9e02c2354cf1d774ba6161ca180c4a47854
= <=>
2023-02-01 01:08:31 +0200

    Force locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -24,6 +24,14 @@ babel = Babel(app)
 def get_locale():
     """ Determine the best match with supported language
     """
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
