db2eea864c7a51e24236fee304d3217d783ffa3a
= <=>
2023-01-31 21:24:16 +0200

    Add port

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,10 @@ app's module
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
     """ The home page for the website
     """
@@ -17,5 +18,4 @@ def index():
 if __name__ == "__main__":
     """Run the app
     """
-    app.run()
-    app.debug = False
+    app.run(host="0.0.0.0", port=5000)
