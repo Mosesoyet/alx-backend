e38312ccc8dbc2c26ba3018987cfed95d7a9514e
= <=>
2023-02-25 02:17:10 -0800

    Modify

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""
-Task 0's module
+""" Task 0's module
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Return a tuple of size 2 containing a start index 
+    """ Return a tuple of size 2 containing a start index 
     and an end index corresponding to the range of indexs 
     to return in a list for those particular pagination parameters
     """
